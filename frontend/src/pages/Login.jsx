d48baaf3edae0ae4d2837b9f9b0697c3b2fcb049
Sameer Shukla <shuklasameer419@gmail.com>
2026-04-13 12:27:48 +0530

    update/login/singup

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -25,7 +25,7 @@ export default function Login() {
     }
 
     try {
-      const res = await fetch("http://localhost:3000/login", {
+      const res = await fetch("http://localhost:3000/api/login", {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify(form),
